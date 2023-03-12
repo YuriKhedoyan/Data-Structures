210e156054ced2c64bd08c1282cbf3c77efe3571
YuriKhedoyan <97390659+YuriKhedoyan@users.noreply.github.com>
2023-03-12 21:39:22 +0400

    Add linkedList.js

---
+++ LinkedList.js
@@ -0,0 +1,73 @@
+class LinkedList {
+  constructor(head) {
+    this.head = head
+  }
+
+  addItem(elem) {
+    let current = this.head;
+
+    if (current == null) {
+      return this.head = elem;
+    }
+
+    while (current.next) {
+      current = current.next;
+    }
+
+    current.next = elem;
+  }
+
+  size() {
+    if (this.head === null) 0;
+
+    let size = 1;
+    let current = this.head;
+
+    while (current.next) {
+      size += 1;
+      current = current.next
+    }
+
+    return size;
+  }
+
+  removeElemAt(index) {
+    let i = 0;
+    let current = this.head;
+
+    while (current.next) {
+      if (i === index - 1) {
+        current.next = current.next.next;
+      } else {
+        current = current.next
+      }
+
+      i++
+    }
+  }
+
+  updateElemAt(index, newElem) {
+    let i = 0;
+    let current = this.head;
+
+    while (current.next) {
+      if (i === index - 1) {
+        current.next.value = newElem;
+      } else {
+        current = current.next
+      }
+
+      i++
+    }
+  }
+}
+
+let myList = new LinkedList();
+myList.addItem({ value: '', next: null })
+myList.addItem({ value: 1, next: null })
+myList.addItem({ value: false, next: null })
+myList.removeElemAt(2)  //false
+myList.updateElemAt(0, 1) //'' = 1
+myList.size() // 2 
+
+
